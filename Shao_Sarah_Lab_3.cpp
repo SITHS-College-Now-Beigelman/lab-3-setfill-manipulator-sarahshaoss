d44b3ed0d429d53fcb014feb854e288080ef02bb
Sarah <sarahs389@nycstudents.net>
2024-09-30 16:25:33 -0400

    c

--- Shao_Sarah_Lab_3.cpp
+++ Shao_Sarah_Lab_3.cpp
@@ -10,15 +10,23 @@ using namespace std;   // shortcut
 int main() //to start the code
 {
     cout << setfill ('#') << setw(81) << "" << endl; 
-    cout << "#" << setfill (' ') << setw(80) << "#"<< endl; //changes it to a space instead of #
-    cout <<"# Ways to access the Task Manager on your Windows computer:" << setw(22)<< "#" << endl;  // first line
-    cout <<"#" << setw(80) << "#" << endl;  // spaces for box
-    cout <<"#       Press the key combination Ctrl + Shift + Escape" << setw(26) << "#"<< endl; // changed the extra spaces into setw too
-    cout <<"#" << setw(80) << "#" << endl;  // spaces for box
+    //changes fill to # for the top line
+    cout << "#" << setfill (' ') << setw(80) << "#"<< endl; 
+    //changes it to a space instead of #
+    cout <<"# Ways to access the Task Manager on your Windows computer:" << setw(22)<< "#" << endl;  
+    // first line
+    cout <<"#" << setw(80) << "#" << endl;  
+    // spaces for box
+    cout <<"#       Press the key combination Ctrl + Shift + Escape" << setw(26) << "#"<< endl; 
+    // changed the extra spaces into setw too
+    cout <<"#" << setw(80) << "#" << endl;  
+    // spaces for box
     cout <<"#       Press the key combination Ctrl + Alt + Delete and select \"Task Manager\" #"<< endl;
-    cout <<"#" << setw(80) << "#" << endl;  // spaces for box
+    cout <<"#" << setw(80) << "#" << endl;  
+    // spaces for box
     cout <<"#       Type \"Task Manager\" in the Windows Start menu search" << setw(21) << "#"<< endl;
-    cout <<"#" << setw(80) << "#" << endl;  // spaces for box
+    cout <<"#" << setw(80) << "#" << endl;  
+    // spaces for box
     cout <<"#################################################################################" << endl;
 
 return 0; //returns code
